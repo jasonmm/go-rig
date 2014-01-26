144ec71a38a7ce68174d3e82cf0c61fbbb566e4f
Jason <jasonmm.github@gmail.com>
2014-01-25 21:28:55 -0600

    Added formatted output and code comments.

--- rig.go
+++ rig.go
@@ -6,9 +6,11 @@ import (
 	"github.com/jasonmm/gowc/libgowc"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
+// Default filenames.
 const (
 	FNAMES  = "/usr/share/rig/fnames.idx"
 	MNAMES  = "/usr/share/rig/mnames.idx"
@@ -17,6 +19,8 @@ const (
 	STREET  = "/usr/share/rig/street.idx"
 )
 
+// Get the specified line number from the given file.  If the given line number
+// is larger than the number of lines in the file then the last line is returned.
 func getLine(filePath string, lineNum int) string {
 	cnt := 0
 
@@ -37,6 +41,7 @@ func getLine(filePath string, lineNum int) string {
 	return scanner.Text()
 }
 
+// Return the libgowc.Metrics struct for the given file.
 func countLines(filePath string) libgowc.Metrics {
 	metric, err := libgowc.ProcessSingleFile(filePath)
 	if err != nil {
@@ -45,31 +50,56 @@ func countLines(filePath string) libgowc.Metrics {
 	return metric
 }
 
-func main() {
+// Break out the city/state/zip and create a phone number from the area code.
+func getCityStateZipPhone(loc string) (string, string, string, string) {
+	parts := strings.Split(loc, " ")
 
+	phone := "(" + parts[2] + ") xxx-xxxx"
+
+	return parts[0], parts[1], parts[3], phone
+}
+
+func main() {
+	// Get the number of lines in all the data files.
 	femaleMetric := countLines(FNAMES)
 	maleMetric := countLines(MNAMES)
 	lastNameMetric := countLines(LNAMES)
 	locMetric := countLines(LOCDATA)
 	streetMetric := countLines(STREET)
 
+	// Seed the random number generator.
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	// Get the line number we will choose from.
 	femaleIndex := rand.Intn(femaleMetric.Lines)
 	maleIndex := rand.Intn(maleMetric.Lines)
 	lastNameIndex := rand.Intn(lastNameMetric.Lines)
 	locIndex := rand.Intn(locMetric.Lines)
 	streetIndex := rand.Intn(streetMetric.Lines)
 
+	// Get the chosen random line from the files.
 	femaleName := getLine(FNAMES, femaleIndex)
 	maleName := getLine(MNAMES, maleIndex)
 	lastName := getLine(LNAMES, lastNameIndex)
 	locStr := getLine(LOCDATA, locIndex)
 	street := getLine(STREET, streetIndex)
 
-	fmt.Println(femaleName)
-	fmt.Println(maleName)
-	fmt.Println(lastName)
-	fmt.Println(locStr)
-	fmt.Println(street)
+	// Determine if we display a male or female first name.
+	name := femaleName
+	if rand.Intn(2) == 1 {
+		name = maleName
+	}
+
+	// Create a street number.
+	streetNumber := rand.Intn(9900) + 100
+
+	// Get city/state/zip and phone number.
+	city, state, zip, phone := getCityStateZipPhone(locStr)
+
+	// Output the identity.
+	fmt.Println(name, lastName)
+	fmt.Println(streetNumber, street)
+	fmt.Println(city, state, zip)
+	fmt.Println(phone)
+	fmt.Println()
 }
